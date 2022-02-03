035a974fd7e4842d08605fd6446ceb836e468e5f
danieelfcr <danielfcab2002@gmail.com>
2022-02-02 19:18:47 -0600

    Add Controller

---
+++ LAB00-DFCR1117121/Controllers/ClientsController.cs
@@ -0,0 +1,89 @@
+﻿using LAB00_DFCR1117121.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LAB00_DFCR1117121.Controllers
+{
+    public class ClientsController : Controller
+    {
+        // GET: ClientsController
+        public ActionResult Index()
+        {
+            var list = new List<Client>();
+            return View(list);  //Se utiliza /clients
+        }
+
+        // GET: ClientsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ClientsController/Create
+        public ActionResult Create()
+        {
+            return View(new Client());
+        }
+
+        // POST: ClientsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ClientsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ClientsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ClientsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ClientsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
