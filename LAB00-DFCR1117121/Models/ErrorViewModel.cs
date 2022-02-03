064650068d99a7e9f7b67a6e18f94c27bfcdcdac
danieelfcr <danielfcab2002@gmail.com>
2022-02-02 18:44:57 -0600

    Creation

---
+++ LAB00-DFCR1117121/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LAB00_DFCR1117121.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
